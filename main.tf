088ca13a69377d27b5daef2975a289cdb7924d8e
rohan.chandane@gmail.com <rohan.chandane@gmail.com>
2023-06-28 23:09:08 +0100

    updated main with new provider, variable, resource and output

--- main.tf
+++ main.tf
@@ -1,9 +1,24 @@
-# Provider configuration
-provider "null" {}
-
-# Resource configuration
-resource "null_resource" "hello" {
-  provisioner "local-exec" {
-    command = "echo 'Hello, World!'"
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
   }
+  required_version = ">= 1.1.0"
+}
+
+
+variable "name_length" {
+  description = "The number of words in the pet name"
+  default     = "3"
+}
+
+resource "random_pet" "pet_name" {
+  length    = var.name_length
+  separator = "-"
+}
+
+output "pet_name" {
+  value = random_pet.pet_name.id
 }
